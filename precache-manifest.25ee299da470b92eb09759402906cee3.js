1844efba563a6ac80cf84935c57a87a5956993d9
James Antley <jimmy.antley@gmail.com>
2021-08-22 22:39:55 -0400

    Updates

---
+++ precache-manifest.25ee299da470b92eb09759402906cee3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb2f0506b7f8e62f29e860e00a737bc7",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "d39b5cbf42158efe543c",
+    "url": "/React_Portfolio/static/css/main.abc08d73.chunk.css"
+  },
+  {
+    "revision": "0480b49fc3c22da481e2",
+    "url": "/React_Portfolio/static/js/2.177df0c9.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/React_Portfolio/static/js/2.177df0c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d39b5cbf42158efe543c",
+    "url": "/React_Portfolio/static/js/main.09f6aefe.chunk.js"
+  },
+  {
+    "revision": "8ce534014186303edb41",
+    "url": "/React_Portfolio/static/js/runtime-main.b3be291f.js"
+  },
+  {
+    "revision": "456d27d30afacf5ef9c2d04e25452c99",
+    "url": "/React_Portfolio/static/media/mock1.456d27d3.png"
+  },
+  {
+    "revision": "37e11540fe7357463bd40d3effb35da1",
+    "url": "/React_Portfolio/static/media/mock2.37e11540.png"
+  },
+  {
+    "revision": "6a736fcd570ebcf9699f8d5e9f949f60",
+    "url": "/React_Portfolio/static/media/mock3.6a736fcd.png"
+  },
+  {
+    "revision": "bd6a383d15853619853b9eba50524095",
+    "url": "/React_Portfolio/static/media/mock4.bd6a383d.png"
+  },
+  {
+    "revision": "601dc99f54f0a0ba539e2bdc01912081",
+    "url": "/React_Portfolio/static/media/self.601dc99f.png"
+  }
+]);
